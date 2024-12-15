4036201b2f605b6094b3f8efbfacb6f5ec66a780
Jacobsadi <jacobsadi03@gmail.com>
2024-12-15 21:53:33 +0800

    setting up the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+
+import {
+  CustomerReviews,
+  Footer, Hero, PopularProducts, Services, SpeacialOffer, Subscribe,
+  SuperQuality
+} from './sections';
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <main className="relative ">
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero/>
+    </section>
+    <section className="padding ">
+      <PopularProducts />
+    </section>
+    <section className="padding ">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10 ">
+      <Services />
+    </section>
+    <section className="padding ">
+      <SpeacialOffer />
+    </section>
+    <section className="bg-pale-blue padding ">
+      < CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full ">
+      <Subscribe />
+    </section>
+    <section className="padding-x bg-black padding-t pb-8  ">
+      <Footer />
+    </section>
+  </main>
   )
 }
---
+++ src/sections/SpeacialOffer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SpeacialOffer = () => {
+  return <div>SpeacialOffer</div>;
+};
+
+export default SpeacialOffer;
