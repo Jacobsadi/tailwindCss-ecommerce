bb627d9e53f969cedf8e15e19c65a00b981d946c
Jacobsadi <jacobsadi03@gmail.com>
2024-12-16 23:18:38 +0800

    adding the state to change the pictures

---
+++ src/components/ShoeCard.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+type Props = {
+  imgURL: any;
+  changeBigShoeImage: any;
+  bigShoeImg: any;
+};
+const ShoeCard: React.FC<Props> = ({
+  imgURL,
+  changeBigShoeImage,
+  bigShoeImg,
+}) => {
+  function handleClick() {
+    if (bigShoeImg !== imgURL.bigShoe) changeBigShoeImage(imgURL.bigShoe);
+  }
+  return (
+    <div
+      className={`border-2 rounded-xl  max-sm:flex-1 cursor-pointer
+         ${bigShoeImg === imgURL ? "border-coral-red" : "border-transparent"}`}
+      onClick={handleClick}
+    >
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img
+          src={imgURL.thumbnail}
+          alt="Shoe imag"
+          width={127}
+          height={103}
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ShoeCard;
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { arrowRight } from "../assets/icons";
 import { bigShoe1 } from "../assets/images";
 import Button from "../components/Button";
-import { statistics } from "../constants";
+import ShoeCard from "../components/ShoeCard";
+import { shoes, statistics } from "../constants";
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
   return (
     <section
       className=" w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
@@ -37,14 +39,25 @@ const Hero = () => {
         </div>
       </div>
 
-      <div>
+      <div className=" relative flex items-center justify-center flex-col flex-1 xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
-          src={bigShoe1}
+          src={bigShoeImg}
           alt="Shoe Collection"
           width={610}
           height={500}
           className="object-contain relative z-10"
         />
+        <div className=" flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe, index) => (
+            <div key={index}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
