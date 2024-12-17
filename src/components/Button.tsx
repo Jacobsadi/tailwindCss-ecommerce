00823f225ced234fa2c7cf0d7779ce1f472dbeda
Jacobsadi <jacobsadi03@gmail.com>
2024-12-17 19:00:38 +0800

    adding the quality design

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,11 +9,13 @@ const Button: React.FC<ButtonProps> = ({ label, iconURL }) => {
   return (
     <button className="flex items-center gap-2 px-7 py-4 leading-none bg-coral-red text-lg text-white rounded-full">
       <span>{label}</span>
-      <img
-        src={iconURL}
-        alt="Button Icon"
-        className="w-5 h-5 ml-2 rounded-full"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="Button Icon"
+          className="w-5 h-5 ml-2 rounded-full"
+        />
+      )}
     </button>
   );
 };
