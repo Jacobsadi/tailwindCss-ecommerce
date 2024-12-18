2381f0dc2b3e9fddd14d6f9863895e285c8488fb
Jacobsadi <jacobsadi03@gmail.com>
2024-12-18 15:26:34 +0800

    adding special offers design

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,17 +3,36 @@ import React from "react";
 type ButtonProps = {
   label: string;
   iconURL: string;
+  backgroundColor: string;
+  borderColor: string;
+  textColor: string;
+  fullWidth: string;
 };
 
-const Button: React.FC<ButtonProps> = ({ label, iconURL }) => {
+const Button: React.FC<ButtonProps> = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className="flex items-center gap-2 px-7 py-4 leading-none bg-coral-red text-lg text-white rounded-full">
-      <span>{label}</span>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
+      {label}
+
       {iconURL && (
         <img
           src={iconURL}
-          alt="Button Icon"
-          className="w-5 h-5 ml-2 rounded-full"
+          alt="arrow right icon"
+          className="ml-2 rounded-full bg-white w-5 h-5"
         />
       )}
     </button>
